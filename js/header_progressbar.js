afd73a9ea16cd563da1976bddd334b42db62b3dd
Alfred <ya95123@gmail.com>
2020-12-16 16:08:21 +0800

    fix RWD bug

--- js/header_progressbar.js
+++ js/header_progressbar.js
@@ -1,7 +1,7 @@
 const progressbar = document.getElementById("h-progressbar")
-const bodyHeight = document.body.scrollHeight - window.innerHeight
 
 const detectBar = () => {
+  const bodyHeight = document.body.scrollHeight - window.innerHeight
   // scrollTop 被頁面捲上去的距離高度
   let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
   let res = scrollTop * 100 / bodyHeight
