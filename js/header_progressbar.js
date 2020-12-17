c319a91c29e9b1ddafa79105538ae5aa1513656d
Alfred <ya95123@gmail.com>
2020-12-17 11:41:35 +0800

    optimization code

--- js/header_progressbar.js
+++ js/header_progressbar.js
@@ -3,7 +3,7 @@ const progressbar = document.getElementById("h-progressbar")
 const detectBar = () => {
   const bodyHeight = document.body.scrollHeight - window.innerHeight
   // scrollTop 被頁面捲上去的距離高度
-  let scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
+  const scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
   let progress = scrollTop * 100 / bodyHeight
   progressbar.style.width = `${progress}%`
 }
